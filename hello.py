6b5ff4486650d46589471e093cc15823a323b29b
Patricia Magalhães <paath.magalhaes2@gmail.com>
2022-05-07 16:34:57 -0300

    Inserindo dados dos produtos na API

--- hello.py
+++ hello.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from urllib import response
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return "Hello, World!"
+
+
+@app.route('/products')
+def products():
+    response = jsonify([
+        {
+            "title": "Caneca personalizada de Porcelana Backend",
+            "amount": 123.45,
+            "installments": {"number": 3, "total": 41.45, "hasFee": True},
+        },
+        {
+            "title": "Caneca de Tulipa",
+            "amount": 123.45,
+            "installments": {"number": 3, "total": 41.45},
+        }
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
